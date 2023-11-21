65cd868e496bd8fd6dab9e2ab3c0bc93204903ee
Tara Dhesi <104512328+taradhesi@users.noreply.github.com>
2023-11-21 18:31:24 +0000

    Homepage
    Includes the navbar to be added onto the other pages.

---
+++ Homepage/script.js
@@ -0,0 +1,7 @@
+function showCartDropdown() {
+    document.getElementById("cart-dropdown").style.display = "block";
+}
+
+function hideCartDropdown() {
+    document.getElementById("cart-dropdown").style.display = "none";
+}
