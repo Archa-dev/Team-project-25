d59b56ca1be34e37884bd6d9eca1365f3f9d9774
MZaman8 <148326306+MZaman8@users.noreply.github.com>
2023-11-30 18:24:19 +0000

    Update login.php
    replaced js with php, changed the forms so they support php functions

--- login.php
+++ login.php
@@ -10,53 +10,63 @@
 </head>
 
 <body>
-    <script>
-      const cyrb53a = function(str, seed = 0) {                   //hashing algorithm from https://github.com/bryc/code/blob/master/jshash/experimental/cyrb53.js
-    let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
-    for(let i = 0, ch; i < str.length; i++) {
-      ch = str.charCodeAt(i);
-      h1 = Math.imul(h1 ^ ch, 0x85ebca77);
-      h2 = Math.imul(h2 ^ ch, 0xc2b2ae3d);
-    }
-    h1 ^= Math.imul(h1 ^ (h2 >>> 15), 0x735a2d97);
-    h2 ^= Math.imul(h2 ^ (h1 >>> 15), 0xcaf649a9);
-    h1 ^= h2 >>> 16; h2 ^= h1 >>> 16;
-      return 2097152 * (h2 >>> 0) + (h1 >>> 11);
-  };
-    </script>
-    <script>
-      const savedPassword = [6629065793880233];
-const savedEmail = ["test@test.com"];
+    <form method="post" action="login.php">
+        <h2>Username:</h2>
+        <input type="text" name="username">
+        <h2>Password:</h2>
+        <input type="text" name="password"><br>
+        <input type="submit" name="submit">
+        <input type="hidden" name="logsub" value="TRUE">
+    </form>
 
-function checkLogin(){
-var email = document.getElementsByName("inputEmail")[0].value;                  //takes values from the html form
-var password = document.getElementsByName("inputPassword")[0].value;
-
-if (savedEmail.includes(email)){
-    if (savedPassword.includes(cyrb53a(password))){  //needs to be changed to if password is equal to the password tied to the email from db
-        alert("Login Successful");
-        window.location.replace("https://google.com")  //temporary redirect to google as a placeholder until I know the path for mainpage
-        return true;                                  //also needs to send the account details to the main page 
-
+    
+    <?php
+    require_once('connectdb.php');
+    if(isset($_POST['logsub'])){
+      $user=$_POST['username'];
+      $pass=$_POST['password'];
+    
+    
+    if($user==null || $pass==null){
+      echo("enter username and password");
     }
     else{
-        alert("Password Incorrect, Please Try Again");
-        return false;
-    }
-}
-else{
-    alert("Email Incorrect, Please Try Again");
-    return false;
-}
+      $login=$db->prepare('SELECT password FROM logindetails WHERE username=?');
+      $login->bindParam(1,$user);
 
-}
-    </script>
-    <form onsubmit="return checkLogin()" class="login-form">
-        <h2>Email:</h2>
-        <input type="text" name="inputEmail">
-        <h2>Password:</h2>
-        <input type="text" name="inputPassword"><br>
-        <input type="submit">
-    </form>
+      $login->execute();
+      $Apass=$login->fetch();
+
+      if($login->rowCount()>0){
+        if(password_verify($pass,$Apass['password'])){
+          echo('success');
+          
+          $sessionid=$db->prepare('SELECT user_id FROM logindetails WHERE username=?');
+          $sessionid->bindParam(1,$user);
+          
+          $sessionid->execute();
+          $Asessionid=$sessionid->fetch();
+
+          session_start();
+          $_SESSION["username"]=$user;
+          $_SESSION["user_id"]=$Asessionid['user_id'];
+          
+          header("Location:index.php");
+          exit();
+        
+        }else{
+          echo('incorrect password');
+        }
+
+
+      }else{
+        echo('incorrect user');
+      }
+
+    }
+
+  }
+    ?>
+
 </body>
 </html>
