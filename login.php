a292a607e9add61384e4ca675a8f3c14864bd6b7
EthanDockerty <148327639+EthanDockerty@users.noreply.github.com>
2023-11-30 16:30:28 +0000

    merged js from individual files to embedded in page

--- login.php
+++ login.php
@@ -10,8 +10,47 @@
 </head>
 
 <body>
-    <script src="hash.js"></script>
-    <script src="loginCheck.js"></script>
+    <script>
+      const cyrb53a = function(str, seed = 0) {                   //hashing algorithm from https://github.com/bryc/code/blob/master/jshash/experimental/cyrb53.js
+    let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
+    for(let i = 0, ch; i < str.length; i++) {
+      ch = str.charCodeAt(i);
+      h1 = Math.imul(h1 ^ ch, 0x85ebca77);
+      h2 = Math.imul(h2 ^ ch, 0xc2b2ae3d);
+    }
+    h1 ^= Math.imul(h1 ^ (h2 >>> 15), 0x735a2d97);
+    h2 ^= Math.imul(h2 ^ (h1 >>> 15), 0xcaf649a9);
+    h1 ^= h2 >>> 16; h2 ^= h1 >>> 16;
+      return 2097152 * (h2 >>> 0) + (h1 >>> 11);
+  };
+    </script>
+    <script>
+      const savedPassword = [6629065793880233];
+const savedEmail = ["test@test.com"];
+
+function checkLogin(){
+var email = document.getElementsByName("inputEmail")[0].value;                  //takes values from the html form
+var password = document.getElementsByName("inputPassword")[0].value;
+
+if (savedEmail.includes(email)){
+    if (savedPassword.includes(cyrb53a(password))){  //needs to be changed to if password is equal to the password tied to the email from db
+        alert("Login Successful");
+        window.location.replace("https://google.com")  //temporary redirect to google as a placeholder until I know the path for mainpage
+        return true;                                  //also needs to send the account details to the main page 
+
+    }
+    else{
+        alert("Password Incorrect, Please Try Again");
+        return false;
+    }
+}
+else{
+    alert("Email Incorrect, Please Try Again");
+    return false;
+}
+
+}
+    </script>
     <form onsubmit="return checkLogin()" class="login-form">
         <h2>Email:</h2>
         <input type="text" name="inputEmail">
