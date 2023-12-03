6c616f6eb20a23431cc95ea1d09f4a88957cea01
MZaman8 <148326306+MZaman8@users.noreply.github.com>
2023-12-03 14:22:56 +0000

    Update signup.php
    changed input types of password and email to be password and email, added database integration with register, added address, name and username fields

--- signup.php
+++ signup.php
@@ -22,21 +22,7 @@
 }
 </style>
 <body>
-    <script>
-      const cyrb53a = function(str, seed = 0) {                   //hashing algorithm from https://github.com/bryc/code/blob/master/jshash/experimental/cyrb53.js
-    let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
-    for(let i = 0, ch; i < str.length; i++) {
-      ch = str.charCodeAt(i);
-      h1 = Math.imul(h1 ^ ch, 0x85ebca77);
-      h2 = Math.imul(h2 ^ ch, 0xc2b2ae3d);
-    }
-    h1 ^= Math.imul(h1 ^ (h2 >>> 15), 0x735a2d97);
-    h2 ^= Math.imul(h2 ^ (h1 >>> 15), 0xcaf649a9);
-    h1 ^= h2 >>> 16; h2 ^= h1 >>> 16;
-      return 2097152 * (h2 >>> 0) + (h1 >>> 11);
-  };
-    </script>
-    <script>
+<script>
   const savedEmails = [];
   const savedPasswords = [];
 
@@ -44,8 +30,10 @@
 function signupSuccess(){
       var email = document.getElementsByName("inputEmail")[0].value;                  //takes values from the html form
       var password = document.getElementsByName("inputPassword")[0].value;
-      
-      if (email.length > 0) {
+      var user=document.getElementsByName("Username")[0].value;
+      var name=document.getElementsByName("Name")[0].value;
+
+      if (email.length > 0 && user.length > 0 && name.length > 0 && user.length > 0) {
           if (password.length > 5){                    // currently trusts the user to input a valid email, probably should be changed
               savedEmails.push(email);
               savedPasswords.push(password);           // to be replaced with appending saved details to the accounts database
@@ -57,19 +45,63 @@ function signupSuccess(){
           }
       }
       else{
-          alert("Invalid email");
+          alert("Invalid email/username/name");
           return false;
       }
 
   }
     </script>
-    <form onsubmit="return signupSuccess()" class="signup-form">
+    <form method="post" action="signup.php" onsubmit="return signupSuccess()" class="signup-form">
         <h2>Email:</h2>
-        <input type="text" name="inputEmail">
+        <input type="email" name="inputEmail">
+        <h2>Username:</h2>
+        <input type="text" name="Username">
+        <h2>Name:</h2>
+        <input type="text" name="Name">
+        <h2>Address (optional):</h2>
+        <input type="text" name="Address">
         <h2>Password:</h2>
         <p>Please make sure password is 6+ characters</p>
-        <input type="text" name="inputPassword"><br>
+        <input type="password" name="inputPassword"><br>
         <input type="submit">
+        <input type="hidden" name="sub">
     </form>
+    <?php
+    if(isset($_POST['sub'])){
+    require_once('connectdb.php');
+    $email=$_POST['inputEmail'];
+    $user=$_POST['Username'];
+    $name=$_POST['Name'];
+    $pass=$_POST['inputPassword'];
+    $add=$_POST['Address'];
+    $auth="customer";
+    $pass=password_hash($pass,PASSWORD_DEFAULT);
+    
+    $check=$db->prepare("SELECT * FROM logindetails WHERE username=?");
+    $check->bindParam(1,$user);
+    $check->execute();
+
+    if($check->rowCount()>0){
+      echo ("Username already exists");
+    }else{
+    $rej=$db->prepare("INSERT INTO logindetails (username,password,email,authorization_level)value(?,?,?,?)");
+    $rej->bindParam(1,$user);
+    $rej->bindParam(2,$pass);
+    $rej->bindParam(3,$email);
+    $rej->bindParam(4,$auth);
+    if($rej->execute()){
+      $id=$db->lastInsertId();
+      $cdetails=$db->prepare("INSERT INTO customerdetails(user_id,name,default_address) value (?,?,?)");
+      $cdetails->bindParam(1,$id);
+      $cdetails->bindParam(2,$name);
+      $cdetails->bindParam(3,$add);
+      $cdetails->execute();
+      echo("Successful");
+    }else{
+      echo ("Unsuccessful");
+    }
+    }
+    }
+    ?>
 </body>
 </html>
