f7d9cd4f575c56b0ed1c6ee2f0304d708924bee8
noorulain9 <148326760+noorulain9@users.noreply.github.com>
2023-12-08 12:36:12 +0000

    Update login.php
    Added the style in it

--- login.php
+++ login.php
@@ -3,26 +3,283 @@
 
 <head>
 
-  <meta charset="UTF-8" />
+<meta charset="UTF-8" />
+    <title>Login - SHADED</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+        crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    
   <title>Login</title>
   <h1>Login</h1>
 
 </head>
 <style>
-  body {
-    font-family: "Century Gothic", sans-serif;
-    background-color: #ffffff;
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    outline: none; border: none;
-    text-decoration: none;
-    text-transform: capitalize;
-    transition: .2s linear;
+      html {
+    font-size: 100%;
+    scroll-behavior: smooth;
+      }
+
+        body {
+            font-family: "Century Gothic", sans-serif;
+            background-color: #ffffff;
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            outline: none;
+            border: none;
+            text-decoration: none;
+            text-transform: capitalize;
+            transition: .2s linear;
+        }
+
+        header {
+          background: #ffffff;
+            position: fixed;
+            width: 100%;
+            z-index: 1000;
+            display: flex;
+            justify-content: space-between; /* Align logo to the left and nav to the right */
+            align-items: center;
+            top: 0; left: 0; right: 0;
+            box-shadow: 0 .5rem 1rem rgba(0, 0, 0, 0.1);
+        }
+
+        .navbar a {
+                font-size: 15px;
+                color: #000000;
+                text-decoration: none;
+            }
+
+            /* Hide the dropdown arrow */
+            .navbar-nav .nav-item.dropdown > .nav-link::after {
+                display: none !important
+            }
+
+.logo img {
+    max-width: 100%; /* Ensure the logo scales proportionally */
+    max-height: 50px; /* Set the maximum height as needed */
+    margin-left: auto; /* Center the logo horizontally */
 }
+
+.fas {
+    font-size: 15px;
+}
+
+main {
+            margin-top: 8vh;
+            display: flex;
+            justify-content: center;
+            align-items: flex-start;
+            height: 100%;
+        }
+
+        .sticky-footer-padding {
+    margin-bottom: 10vh;
+    /* Adjust the margin bottom to match the height of the footer */
+}
+
+/*Sign Up Styles by Maryam*/
+.login-form {
+            width: 100%;
+            max-width: 600px;
+            padding: 20px;
+            border: 1px solid #ccc;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+.login-form input,
+        .login-form button {
+            margin-bottom: 5px;
+            font-size: 14px; /* Adjust the font size as needed */
+        }
+
+        .login-form h2 {
+            font-size: 16px; /* Adjust the font size for all h2 elements */
+        }
+        .login-form a {
+            display: block;
+            text-align: center;
+            font-size: 14px; /* Adjust the font size for the "Already have an account?" link */
+        }
+
+        .sticky-footer-padding {
+            margin-bottom: 8vh;
+            height: 100%; /* Take full height */
+        }
+        
+/* Updated Footer Styles */
+.footer {
+            background-color: #fff;
+            color: grey;
+            padding: 10px;
+            text-align: center;
+            position: fixed;
+            bottom: 0;
+            left: 0;
+            width: 100%;
+            font-size: 14px;
+            box-shadow: 0 -5px 10px rgba(0, 0, 0, 0.1);
+        }
+
+.social-icons a {
+            margin: 0 20px;
+            color: #000;
+            font-size: 14px;
+        }
+
+.terms-links a {
+    margin-left: 5px;
+    color: #6c757d; /* Change the color as needed */
+    text-decoration: none;
+}
+
+.terms-links a:hover {
+    text-decoration: underline; /* Add underlining on hover if desired */
+    color: #000; /* Change the hover color as needed */
+}
+
 </style>
+
 <body>
-    <form method="post" action="login.php">
+<header>
+        <!-- added bootstrap navbar utility classes -->
+        <nav class="navbar navbar-expand-sm w-100">
+
+            <!-- using container-fluid for responsiveness -->
+            <div class="container-fluid">
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMenuItems" aria-controls="navbarMenuItems" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <a href="#" class="navbar-brand logo">
+                    <img src="shaded logo.png" alt="Shaded Logo">
+                </a>
+                <div class="collapse navbar-collapse" id="navbarMenuItems">
+
+                    <!-- navbar to the left of the search box -->
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Men
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Men Category 1</a></li>
+                                <li><a class="dropdown-item" href="#">Men Category 2</a></li>
+                                <li><a class="dropdown-item" href="#">Men Category 3</a></li>
+                                <li><a class="dropdown-item" href="#">Men Category 4</a></li>
+                                <li><a class="dropdown-item" href="#">Men Category 5</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Women
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Women Category 1</a></li>
+                                <li><a class="dropdown-item" href="#">Women Category 2</a></li>
+                                <li><a class="dropdown-item" href="#">Women Category 3</a></li>
+                                <li><a class="dropdown-item" href="#">Women Category 4</a></li>
+                                <li><a class="dropdown-item" href="#">Women Category 5</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Unisex
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Unisex Category 1</a></li>
+                                <li><a class="dropdown-item" href="#">Unisex Category 2</a></li>
+                                <li><a class="dropdown-item" href="#">Unisex Category 3</a></li>
+                                <li><a class="dropdown-item" href="#">Unisex Category 4</a></li>
+                                <li><a class="dropdown-item" href="#">Unisex Category 5</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Prescription
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Prescription Category 1</a></li>
+                                <li><a class="dropdown-item" href="#">Prescription Category 2</a></li>
+                                <li><a class="dropdown-item" href="#">Prescription Category 3</a></li>
+                                <li><a class="dropdown-item" href="#">Prescription Category 4</a></li>
+                                <li><a class="dropdown-item" href="#">Prescription Category 5</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Blue Light
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Blue Light Category 1</a></li>
+                                <li><a class="dropdown-item" href="#">Blue Light Category 2</a></li>
+                                <li><a class="dropdown-item" href="#">Blue Light Category 3</a></li>
+                                <li><a class="dropdown-item" href="#">Blue Light Category 4</a></li>
+                                <li><a class="dropdown-item" href="#">Blue Light Category 5</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+
+                    <!-- search box -->
+                    <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" id="mySearchInput">
+                        <button class="btn btn-outline-bg" type="submit">
+                            <a href="#">
+                                <i class="fas fa-search"></i>
+                            </a>
+                        </button>
+                    </form>
+
+                    <!-- navbar to the right of the search box -->
+                    <ul class="navbar-nav mw-auto mb-2 mb-lg-0">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <i class="fas fa-user"></i> <!-- Assuming a user icon for admin/user -->
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-end">
+                                <li><a class="dropdown-item" href="#">My Profile</a></li>
+                                <li><a class="dropdown-item" href="#">My Orders</a></li>
+                                <li><a class="dropdown-item" href="#">Logout</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <i class="fas fa-lock"></i> <!-- Assuming a lock icon for log in/sign up -->
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-end">
+                                <li><a class="dropdown-item" href="#">Log In/ Sign Up</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <i class="fas fa-heart"></i>
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-end">
+                                <li><a class="dropdown-item" >Your wish list is empty</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <i class="fas fa-shopping-cart"></i>
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-end">
+                                <li><a class="dropdown-item" >Your shopping cart is empty</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+    </header>
+    <main class = "sticky-footer-padding">
+    <div class="container mt-5">
+      <div class="row justify-content-center">
+         <div class="col-md-6">
+    <form method="post" action="login.php" class="login-form">
         <h2>Username:</h2>
         <input type="text" name="username">
         <h2>Password:</h2>
@@ -30,12 +287,12 @@
         <input type="submit" name="submit">
         <input type="hidden" name="logsub" value="TRUE">
     </form>
-  <form>
-</br>
-</br>
-<a href="signup.php">Dont have an account?</a>
-    </form>
-    
+    <a href="signup.php">Dont have an account?</a>
+    </div>
+        </div>
+      </div>
+      
+  
     <?php
     require_once('connectdb.php');
     if(isset($_POST['logsub'])){
@@ -83,6 +340,32 @@
 
   }
     ?>
-
+<div class="container-fluid">
+        <footer class="footer">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="footer-text">
+                        <p>&copy;Shaded-2023 | All Rights Reserved</p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="social-icons">
+                        <!-- Add your social media icons  -->
+                        <a href="https://www.facebook.com/" target="_blank"><i class="fab fa-facebook"></i></a>
+                        <a href="https://twitter.com/" target="_blank"><i class="fab fa-twitter"></i></a>
+                        <a href="https://instagram.com/" target="_blank"><i class="fab fa-instagram"></i></a>
+                        <!-- Add more social media icons as needed -->
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="terms-links">
+                        <a href="#">Terms of Use</a>
+                        <a href="#">Cookies Policy</a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
 </body>
 </html>
+
