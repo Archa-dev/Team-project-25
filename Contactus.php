7e244caf3dc75ece6ced5e5627cfa148d7459bc9
maryam1452 <148329301+maryam1452@users.noreply.github.com>
2023-12-09 09:42:28 +0000

    Update Contactus.php
    Edited the nav bar and added the footer + extra style code - Maryam

--- Contactus.php
+++ Contactus.php
@@ -11,8 +11,13 @@
     integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
     <style>
-        body {
-            font-family: "Century Gothic", sans-serif;
+        
+        html {
+    font-size: 100%;
+    scroll-behavior: smooth;
+
+    > body {
+        font-family: "Century Gothic", sans-serif;
         background-color: #ffffff;
         margin: 0;
         margin-bottom: 60px; /* Adjust this value to match the height of the footer */
@@ -22,10 +27,9 @@
         text-decoration: none;
         text-transform: capitalize;
         transition: .2s linear;
-}
 
-header {
-    background: #ffffff;
+        > header {
+            background: #ffffff;
             position: fixed;
             width: 100%;
             z-index: 1000;
@@ -34,9 +38,8 @@ header {
             align-items: center;
             top: 0; left: 0; right: 0;
             box-shadow: 0 .5rem 1rem rgba(0, 0, 0, 0.1);
-}
 
-.navbar a {
+            .navbar a {
                 font-size: 15px;
                 color: #000000;
                 text-decoration: none;
@@ -46,51 +49,114 @@ header {
             .navbar-nav .nav-item.dropdown > .nav-link::after {
                 display: none !important
             }
-
-/* Logo Styles */
-.logo img {
-    max-width: 100%;
-    max-height: 50px;
-    margin-left: auto;
+        }
+    }
 }
 
-/* Icon Styles */
+.logo img {
+    max-width: 100%; /* Ensure the logo scales proportionally */
+    max-height: 50px; /* Set the maximum height as needed */
+    margin-left: auto; /* Center the logo horizontally */
+}
+
 .fas {
     font-size: 15px;
 }
 
 main {
-    margin-top: 13vh; /* Adjust margin-top to be equal to the height of the header */
+    margin-top: 11vh; /* Adjust margin-top to be equal to the height of the header */
 }
 
 .sticky-footer-padding {
-    margin-bottom: 10vh;
+    margin-bottom: 11vh;
     /* Adjust the margin bottom to match the height of the footer */
 }
 
+/* Welcome Section Styles */
+.welcome-section {
+    padding: 20px;
+    background-color: #f8f9fa; /* Choose a background color for the welcome section */
+}
+
+.welcome-section h2 {
+    font-size: 18px;
+    color: #000;
+}
+
+/* Sidebar Styles */
+.sidebar {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 300px;
+    height: 100%;
+    padding-top: 110px;
+    background-color: #f8f9fa; /* Choose a background color for the sidebar */
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+.sidebar ul {
+    list-style: none;
+    padding-left: 0;
+}
+
+.sidebar .nav-link {
+    padding: 15px 20px;
+    text-decoration: none;
+    color: #000;
+    font-size: 14px; /* Adjust font size as needed */
+    font-weight: bold;
+    transition: background-color 0.3s;
+    display: block;
+}
+
+.sidebar .nav-link:hover {
+    background-color: lightgrey; /* Set a background color for the hover effect */
+    color: #000;
+}
+
+.main-content {
+    margin-left: 350px; /* Adjust this value to match the width of the sidebar */
+}
+
+/* Additional Styling for Active Link */
+.sidebar .nav-link.active {
+    background-color: #f5f5f5; /* Set a background color for the active link */
+    color: #000;
+    position: relative;
+}
+
+.sidebar .nav-link.active::before {
+    content: "";
+    position: absolute;
+    left: 0;
+    top: 0;
+    height: 100%;
+    width: 5px; /* Adjust the width of the vertical line as needed */
+    background-color: #000; /* Set the color of the vertical line */
+}
+
 /* contact us page Styles */
-.container {
-    max-width: 960px;
-   margin: 50px auto;
-   padding: 20px;
-   background-color: #fff;
-   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); 
- }
- .contact-section {
-   margin-top: 20px;
-   padding: 20px;
-   border-top: 1px solid #ccc;
-   border-bottom: 1px solid #ccc;
- }
- .contact-section h2 {
-   font-size: 24px;
- }
- .contact-section p {
-   font-size: 16px;
- }
- .contact-section .service-hours {
-   margin-bottom: 20px;
- }
+.contact-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-start;
+    padding: 10px;
+    max-width: 800px;
+    margin: 50px;
+        }
+
+        .contact-details {
+            max-width: 45%;
+        }
+
+        .contact-details{
+            font-weight: bold;
+        }
+
+        /* Style the contact form */
+        
+
 
  /* Updated Footer Styles */
 .footer {
@@ -270,39 +336,61 @@ main {
         </nav>
     </header>
 
-    <main class="sticky-footer-padding">
-        
-        <div class="container">
+    <main class="sticky-footer-padding main-content">
+    <div class="container">
+    <aside class="sidebar">
+    <div class="welcome-section">
+        <h2>Welcome to your personal area</h2>
+        <!-- You can dynamically replace [Username] with the actual username -->
+    </div>
+    
+    <ul class="nav flex-column">
+        <li class="nav-item">
+            <a class="nav-link" href="accountPage.php">
+                My Profile
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="order-history.php">
+                My Orders
+            </a>
+            <li class="nav-item">
+            <a class="nav-link active" href="Contactus.php">Contact Us</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="homepage.php">
+                Logout
+            </a>
+        </li>
+    </ul>
+</aside>
+
+    <!-- Main content for the contact page -->
+        <div class="contact-container">
+            <div class="contact-details">
+            <h2 class="border-bottom pb-2">Contact Us</h2>
+
             <div class="contact-section">
-              <h2>Contact us</h2>
-              <p>Use one of the following methods to contact us</p>
+                <h3>Call Our Client Service</h3>
+                <p class="service-hours">Contact our Client Service at 800 800 7732 from Monday to Saturday 9:00 am -
+                    8:00 pm, or Sunday 9:00 am - 6:00 pm.</p>
             </div>
-          
+
             <div class="contact-section">
-              <h3>Call our Client Service</h3>
-              <p class="service-hours">To contact our Client Service you can call 800 800 7732 from Monday to Saturday 9:00 am - 8:00 pm, or Sunday 9:00 am - 6:00 pm.</p>
+                <h3>Send Us a Message</h3>
+                <form action="/submit-message" method="post">
+                    <label for="name">Name:</label>
+                    <input type="text" id="name" name="name" required>
+                    <label for="email">Email:</label>
+                    <input type="email" id="email" name="email" required>
+                    <label for="message">Message:</label>
+                    <textarea id="message" name="message" rows="4" required></textarea>
+                    <input type="submit" class="btn btn-primary" value="Submit">
+                </form>
             </div>
-            <div class = "contact-section">
-              <h3>Send us a message</h3>
-              <form action="/submit-message" method="post">
-                  <label for="name">Name:</label>
-                  <input type="text" id="name" name="name" required><br><br>
-                  <label for="email">Email:</label>
-                  <input type="email" id="email" name="email" required><br><br>
-                  <label for="message">Message:</label>
-                  <textarea id="message" name="message" rows="4" cols="50" required></textarea><br><br>
-                  <input type="submit" value="Submit">
-              </form>
-            
+        </div>
+    </div>
             </div>
-            
-          </div>
-           
-
-           
-        
-
-
     </main>
 
     <!-- Bootstrap Container for Footer -->
