e896afd045182a5cfd3f1f991b0d8c85a310e23f
maryam1452 <148329301+maryam1452@users.noreply.github.com>
2023-12-09 08:33:44 +0000

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -231,6 +231,9 @@ h2{
                                 <li><a class="dropdown-item" href="#">Blue Light Category 5</a></li>
                             </ul>
                         </li>
+                       <li class="nav-item">
+                            <a class="nav-link" href="aboutUs.php">About Us</a>
+                        </li>
                     </ul>
 
                     <!-- search box -->
@@ -250,9 +253,9 @@ h2{
                                 <i class="fas fa-user"></i> <!-- Assuming a user icon for admin/user -->
                             </a>
                             <ul class="dropdown-menu dropdown-menu-end">
-                                <li><a class="dropdown-item" href="#">My Profile</a></li>
-                                <li><a class="dropdown-item" href="#">My Orders</a></li>
-                                <li><a class="dropdown-item" href="#">Logout</a></li>
+                                <li><a class="dropdown-item" href="accountPage.php">My Profile</a></li>
+                                <li><a class="dropdown-item" href="order-history.php">My Orders</a></li>
+                                <li><a class="dropdown-item" href="homepage.php">Logout</a></li>
                             </ul>
                         </li>
                         <li class="nav-item dropdown">
@@ -260,7 +263,8 @@ h2{
                                 <i class="fas fa-lock"></i> <!-- Assuming a lock icon for log in/sign up -->
                             </a>
                             <ul class="dropdown-menu dropdown-menu-end">
-                                <li><a class="dropdown-item" href="#">Log In/ Sign Up</a></li>
+                                <li><a class="dropdown-item" href="login.php">Log In</a></li>
+                                <li><a class="dropdown-item" href="signup.php">Sign Up</a></li>
                             </ul>
                         </li>
                         <li class="nav-item dropdown">
