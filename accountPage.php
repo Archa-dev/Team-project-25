7b996501fcf3b7b3ce34130591550cdf98526367
maryam1452 <148329301+maryam1452@users.noreply.github.com>
2023-12-09 00:40:47 +0000

    Update accountPage.php
    Remade the Account/Profile Page - Maryam

--- accountPage.php
+++ accountPage.php
@@ -207,7 +207,7 @@ button {
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
-                <a href="#" class="navbar-brand logo">
+                <a href="homepage.php" class="navbar-brand logo">
                     <img src="shaded logo.png" alt="Shaded Logo">
                 </a>
                 <div class="collapse navbar-collapse" id="navbarMenuItems">
