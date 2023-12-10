196fce9941dca5bcabb20c1fcb17a5c0b6e563eb
maryam1452 <148329301+maryam1452@users.noreply.github.com>
2023-12-10 00:04:48 +0000

    Update Contactus.php
    Updated by adding final touches and organising the page - Maryam

--- Contactus.php
+++ Contactus.php
@@ -1,108 +1,153 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
+<script>
+    function showMessageConfirmation() {
+        alert("Your message has been sent. Thank you for contacting us!");
+        return false; // Prevents the form from submitting
+    }
+</script>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Contact Us - SHADED</title>
    
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-   
-
     <!--bootstrap css-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    
-    <title>Contact Us-SHADED</title>
-</head>
-<body>
 
     <style>
-        body {
-    font-family: "Century Gothic", sans-serif;
-    background-color: #fff;
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    outline: none;
-    border: none;
-    text-decoration: none;
-    text-transform: capitalize;
-    transition: .2s linear;
-    height: 100vh;
-    display: flex;
-     flex-direction: column;
+        
+        html {
+    font-size: 100%;
+    scroll-behavior: smooth;
+
+    > body {
+        font-family: "Century Gothic", sans-serif;
+        background-color: #ffffff;
+        margin: 0;
+        margin-bottom: 60px; /* Adjust this value to match the height of the footer */
+        padding: 0;
+        box-sizing: border-box;
+        outline: none; border: none;
+        text-decoration: none;
+        text-transform: capitalize;
+        transition: .2s linear;
+
+        > header {
+            background: #ffffff;
+            position: fixed;
+            width: 100%;
+            z-index: 1000;
+            display: flex;
+            justify-content: space-between; /* Align logo to the left and nav to the right */
+            align-items: center;
+            top: 0; left: 0; right: 0;
+            box-shadow: 0 .5rem 1rem rgba(0, 0, 0, 0.1);
+
+            .navbar a {
+                font-size: 15px;
+                color: #000000;
+                text-decoration: none;
+            }
+
+            /* Hide the dropdown arrow */
+            .navbar-nav .nav-item.dropdown > .nav-link::after {
+                display: none !important
+            }
+        }
+    }
 }
 
-header {
-    background: #fff;
-    position: fixed;
-    width: 100%;
-    z-index: 1000;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    top: 0;
-    left: 0;
-    right: 0;
-    box-shadow: 0 .5rem 1rem rgba(0, 0, 0, 0.1);
-}
-
-/* Logo Styles */
 .logo img {
-    max-width: 100%;
-    max-height: 50px;
-    margin-left: auto;
+    max-width: 100%; /* Ensure the logo scales proportionally */
+    max-height: 50px; /* Set the maximum height as needed */
+    margin-left: auto; /* Center the logo horizontally */
 }
 
-/* Icon Styles */
 .fas {
     font-size: 15px;
+}
+
+main {
+    margin-top: 11vh; /* Adjust margin-top to be equal to the height of the header */
+}
+
+.sticky-footer-padding {
+    margin-bottom: 0vh;
+    /* Adjust the margin bottom to match the height of the footer */
 }
 
 /* contact us page Styles */
 .container {
    max-width:fit-content;
    margin: 50px auto;
-   padding: 20px;
+   padding: 10px;
    background-color: #fff;
-   
  }
+
  .contact-section1 {
-   padding: 20px;
-   font-size: 15px;
+   padding: 10px;
+   font-size: 14px;
    margin-left: 150px;
  }
  .contact-section2 {
-   padding: 20px;
+   padding: 10px;
    border-top: 1px solid #ccc ;
    border-bottom: 1px solid #ccc;
    display: flex;
-   margin-top: auto;
-   margin-bottom: auto;
+   margin-top: 10px;
  }
+
  .contact-section3 {
-   padding: 20px;
-   border-bottom: 1px solid #ccc;
+   padding: 10px;
    display: flex;
-   margin-top: auto;
-   margin-bottom: auto;
+   margin-top: 10px;
+   margin-right: 230px;
+   font-size: 14px;
  }
  .contact-section h3 {
    flex:1 ;
-   margin-top: 20px;
+   margin-top: 10px;
    margin-left: 150px;
    margin-right: 20px;
-   font-size: 22px;
+   font-size: 20px;
+   font-weight: bold;
  }
  .contact-section p {
     flex: 1;
     margin-right: 100px;
     margin-left: 100px;
-   font-size: 15px;
+   font-size: 14px;
+   margin-top: 10px;
    margin-bottom: 10px;
  }
- 
- 
+
+ label {
+            display: block;
+            margin-bottom: 0.5rem;
+        }
+
+        input,
+        textarea {
+            width: 100%;
+            padding: 0.5rem;
+            border: 1px solid #ced4da;
+            border-radius: 0.25rem;
+        }
+
+        input[type="submit"] {
+            background-color: darkgrey;
+            color: #fff;
+            border: #000;
+            cursor: pointer;
+        }
+
+        input[type="submit"]:hover {
+            background-color: grey;
+        }
 
  /* Updated Footer Styles */
 .footer {
@@ -110,14 +155,12 @@ header {
             color: grey;
             padding: 10px;
             text-align: center;
-            /* position: fixed; */
-            margin-top: auto;
+            position: fixed;
             bottom: 0;
             left: 0;
             width: 100%;
             font-size: 14px;
             box-shadow: 0 -5px 10px rgba(0, 0, 0, 0.1);
-            
         }
 
 .social-icons a {
@@ -136,7 +179,10 @@ header {
     text-decoration: underline; /* Add underlining on hover if desired */
     color: #000; /* Change the hover color as needed */
 }
+
     </style>
+    </head>
+<body>
 
      <!--bootstrap js-->
      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
@@ -156,7 +202,7 @@ header {
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
-                <a href="#" class="navbar-brand logo">
+                <a href="homepage.php" class="navbar-brand logo">
                     <img src="shaded logo.png" alt="Shaded Logo">
                 </a>
                 <div class="collapse navbar-collapse" id="navbarMenuItems">
@@ -223,6 +269,9 @@ header {
                                 <li><a class="dropdown-item" href="#">Blue Light Category 5</a></li>
                             </ul>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="aboutUs.php">About Us</a>
+                        </li>
                     </ul>
 
                     <!-- search box -->
@@ -237,18 +286,31 @@ header {
 
                     <!-- navbar to the right of the search box -->
                     <ul class="navbar-nav mw-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
-                                <i class="fas fa-user"></i>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <i class="fas fa-user"></i> <!-- Assuming a user icon for admin/user -->
                             </a>
+                            <ul class="dropdown-menu dropdown-menu-end">
+                            <li><a class="dropdown-item" href="accountPage.php">My Profile</a></li>
+                                <li><a class="dropdown-item" href="order-history.php">My Orders</a></li>
+                                <li><a class="dropdown-item" href="homepage.php">Logout</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <i class="fas fa-lock"></i> <!-- Assuming a lock icon for log in/sign up -->
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-end">
+                                <li><a class="dropdown-item" href="login.php">Log In</a></li>
+                                <li><a class="dropdown-item" href="signup.php">Sign Up</a></li>
+                            </ul>
                         </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 <i class="fas fa-heart"></i>
                             </a>
                             <ul class="dropdown-menu dropdown-menu-end">
-                                <li><a class="dropdown-item" href="#">Item 1</a></li>
-                                <li><a class="dropdown-item" href="#">Item 2</a></li>
+                                <li><a class="dropdown-item" >Your wish list is empty</a></li>
                             </ul>
                         </li>
                         <li class="nav-item dropdown">
@@ -256,8 +318,7 @@ header {
                                 <i class="fas fa-shopping-cart"></i>
                             </a>
                             <ul class="dropdown-menu dropdown-menu-end">
-                                <li><a class="dropdown-item" href="#">Product 1</a></li>
-                                <li><a class="dropdown-item" href="#">Product 2</a></li>
+                                <li><a class="dropdown-item" >Your shopping cart is empty</a></li>
                             </ul>
                         </li>
                     </ul>
@@ -266,71 +327,71 @@ header {
         </nav>
     </header>
 
-    <main>
-        
-        <div class="container">
-            <div class="contact-section1">
-              <h1><b>Contact us</b></h1>
+    <main class="sticky-footer-padding main-content">
+    <div class="container">
+    
+    <!-- Main content for the contact page -->
+    <div class="contact-section1">
+    <h1>Contact Us</h1>
               <p>Use one of the following methods to contact us</p>
             </div>
            <div class="contact-section">
             <div class="contact-section2">
               <h3>Call our Client Service</h3>
-              <p class="service-hours">To contact our Client Service you can call 800 800 0000 from Monday to Saturday 9:00 am - 8:00 pm, or Sunday 9:00 am - 6:00 pm.</p>
+              <p class="service-hours">To contact our Client Service you can call <strong>800 800 0000</strong> from Monday to Saturday 9:00 am - 8:00 pm, or Sunday 9:00 am - 6:00 pm.</p>
             </div>
-            <div class = "contact-section3">
-              <h3>Send us a message</h3>
-              <p>
-              <form action="/submit-message" method="post">
-                  <label for="name">Name:</label>
-                  <input type="text" id="name" name="name" required><br><br>
-                  <label for="email">Email:</label>
-                  <input type="email" id="email" name="email" required><br><br>
-                  <label for="message">Message:</label>
-                  <textarea id="message" name="message" rows="4" cols="50" required></textarea><br><br>
-                  <input type="submit" value="Submit">
-              </form>
-            </p>
+            <div class="contact-section3">
+            <h3>Send us a message</h3>
+            <form action="/submit-message" method="post" onsubmit="return showMessageConfirmation()">
+        <div class="row mb-3">
+            <div class="col">
+                <label for="name" class="form-label">Name:</label>
+                <input type="text" id="name" name="name" class="form-control" required>
+            </div>
+            <div class="col">
+                <label for="email" class="form-label">Email:</label>
+                <input type="email" id="email" name="email" class="form-control" required>
+            </div>
+        </div>
+        <div class="mb-3">
+            <label for="message" class="form-label">Message:</label>
+            <textarea id="message" name="message" rows="4" class="form-control" required></textarea>
+        </div>
+        <input type="submit" value="Submit" class="btn btn-primary">
+    </form>
             
             </div>
         </div>
-            
           </div>
-           
-
-           
-        
-
-
     </main>
 
-   <!-- Bootstrap Container for Footer -->
-   <div class="container-fluid">
-    <footer class="footer">
-        <div class="row">
-            <div class="col-md-4">
-                <div class="footer-text">
-                    <p>&copy;Shaded-2023 | All Rights Reserved</p>
+    <!-- Bootstrap Container for Footer -->
+    <div class="container-fluid">
+        <footer class="footer">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="footer-text">
+                        <p>&copy;Shaded-2023 | All Rights Reserved</p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="social-icons">
+                        <!-- Add your social media icons  -->
+                        <a href="https://www.facebook.com/" target="_blank"><i class="fab fa-facebook"></i></a>
+                        <a href="https://twitter.com/" target="_blank"><i class="fab fa-twitter"></i></a>
+                        <a href="https://instagram.com/" target="_blank"><i class="fab fa-instagram"></i></a>
+                        <!-- Add more social media icons as needed -->
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="terms-links">
+                        <a href="#">Terms of Use</a>
+                        <a href="#">Cookies Policy</a>
+                    </div>
                 </div>
             </div>
-            <div class="col-md-4">
-                <div class="social-icons">
-                    <!-- Add your social media icons  -->
-                    <a href="https://www.facebook.com/" target="_blank"><i class="fab fa-facebook"></i></a>
-                    <a href="https://twitter.com/" target="_blank"><i class="fab fa-twitter"></i></a>
-                    <a href="https://instagram.com/" target="_blank"><i class="fab fa-instagram"></i></a>
-                    <!-- Add more social media icons as needed -->
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="terms-links">
-                    <a href="#">Terms of Use</a>
-                    <a href="#">Cookies Policy</a>
-                </div>
-            </div>
-        </div>
-    </footer>
-</div>
+        </footer>
+    </div>
 
 </body>
-</html>  
+</html>
