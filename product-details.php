398abf175f44d7f1d431543ff88f0efd0c7c9707
Tara Dhesi <104512328+taradhesi@users.noreply.github.com>
2023-12-10 14:27:17 +0000

    Update product-details.php

--- product-details.php
+++ product-details.php
@@ -59,7 +59,7 @@ html {
 }
 
 .fas {
-    font-size: 15px;
+    font-size: 15px;/* icon */
 }
 
 main {
@@ -112,13 +112,13 @@ main {
 
 .terms-links a {
     margin-left: 5px;
-    color: #6c757d; /* Change the color as needed */
+    color: #6c757d;
     text-decoration: none;
 }
 
 .terms-links a:hover {
-    text-decoration: underline; /* Add underlining on hover if desired */
-    color: #000; /* Change the hover color as needed */
+    text-decoration: underline; /* underlining on hover  */
+    color: #000; /* hover color */
 }
 
 </style>
@@ -303,15 +303,17 @@ main {
                 </div>
                 <div class="col-md-4">
                     <div class="social-icons">
-                        <!-- Add your social media icons  -->
+                        <!-- social media icons  -->
                         <a href="https://www.facebook.com/" target="_blank"><i class="fab fa-facebook"></i></a>
                         <a href="https://twitter.com/" target="_blank"><i class="fab fa-twitter"></i></a>
                         <a href="https://instagram.com/" target="_blank"><i class="fab fa-instagram"></i></a>
-                        <!-- Add more social media icons as needed -->
+                       
                     </div>
                 </div>
                 <div class="col-md-4">
                     <div class="terms-links">
+                      
+                        <!-- links do not redirect anywhere  -->
                         <a href="#">Terms of Use</a>
                         <a href="#">Cookies Policy</a>
                     </div>
