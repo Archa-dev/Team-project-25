aad1573abb5bfef4f7a494705e3dd184df0b0105
maryam1452 <148329301+maryam1452@users.noreply.github.com>
2023-12-10 02:51:26 +0000

    Update accountPage.php
    Remade the Account/Profile Page and added a sidebar - Maryam

--- accountPage.php
+++ accountPage.php
@@ -287,11 +287,11 @@ button {
                                 Men
                             </a>
                             <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Men Category 1</a></li>
-                                <li><a class="dropdown-item" href="#">Men Category 2</a></li>
-                                <li><a class="dropdown-item" href="#">Men Category 3</a></li>
-                                <li><a class="dropdown-item" href="#">Men Category 4</a></li>
-                                <li><a class="dropdown-item" href="#">Men Category 5</a></li>
+                                <li><a class="dropdown-item" href="#">Men's Black Sunglasses</a></li>
+                                <li><a class="dropdown-item" href="#">Men's White Sunglasses</a></li>
+                                <li><a class="dropdown-item" href="#">Men's Yellow Sunglasses</a></li>
+                                <li><a class="dropdown-item" href="#">Men's Brown Sunglasses</a></li>
+                                <li><a class="dropdown-item" href="#">Men's Green Sunglasses</a></li>
                             </ul>
                         </li>
                         <li class="nav-item dropdown">
@@ -299,11 +299,11 @@ button {
                                 Women
                             </a>
                             <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Women Category 1</a></li>
-                                <li><a class="dropdown-item" href="#">Women Category 2</a></li>
-                                <li><a class="dropdown-item" href="#">Women Category 3</a></li>
-                                <li><a class="dropdown-item" href="#">Women Category 4</a></li>
-                                <li><a class="dropdown-item" href="#">Women Category 5</a></li>
+                            <li><a class="dropdown-item" href="#">Women's Black Sunglasses</a></li>
+                                <li><a class="dropdown-item" href="#">Women's White Sunglasses</a></li>
+                                <li><a class="dropdown-item" href="#">Women's Yellow Sunglasses</a></li>
+                                <li><a class="dropdown-item" href="#">Women's Brown Sunglasses</a></li>
+                                <li><a class="dropdown-item" href="#">Women's Green Sunglasses</a></li>
                             </ul>
                         </li>
                         <li class="nav-item dropdown">
@@ -311,11 +311,11 @@ button {
                                 Unisex
                             </a>
                             <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Unisex Category 1</a></li>
-                                <li><a class="dropdown-item" href="#">Unisex Category 2</a></li>
-                                <li><a class="dropdown-item" href="#">Unisex Category 3</a></li>
-                                <li><a class="dropdown-item" href="#">Unisex Category 4</a></li>
-                                <li><a class="dropdown-item" href="#">Unisex Category 5</a></li>
+                                <li><a class="dropdown-item" href="#">Unisex Black Sunglasses</a></li>
+                                <li><a class="dropdown-item" href="#">Unisex White Sunglasses</a></li>
+                                <li><a class="dropdown-item" href="#">Unisex Yellow Sunglasses</a></li>
+                                <li><a class="dropdown-item" href="#">Unisex Brown Sunglasses</a></li>
+                                <li><a class="dropdown-item" href="#">Unisex Green Sunglasses</a></li>
                             </ul>
                         </li>
                         <li class="nav-item dropdown">
@@ -323,11 +323,11 @@ button {
                                 Prescription
                             </a>
                             <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Prescription Category 1</a></li>
-                                <li><a class="dropdown-item" href="#">Prescription Category 2</a></li>
-                                <li><a class="dropdown-item" href="#">Prescription Category 3</a></li>
-                                <li><a class="dropdown-item" href="#">Prescription Category 4</a></li>
-                                <li><a class="dropdown-item" href="#">Prescription Category 5</a></li>
+                                <li><a class="dropdown-item" href="#">Prescription Black Sunglasses</a></li>
+                                <li><a class="dropdown-item" href="#">Prescription White Sunglasses</a></li>
+                                <li><a class="dropdown-item" href="#">Prescription Yellow Sunglasses</a></li>
+                                <li><a class="dropdown-item" href="#">Prescription Brown Sunglasses</a></li>
+                                <li><a class="dropdown-item" href="#">Prescription Green Sunglasses</a></li>
                             </ul>
                         </li>
                         <li class="nav-item dropdown">
@@ -335,11 +335,11 @@ button {
                                 Blue Light
                             </a>
                             <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Blue Light Category 1</a></li>
-                                <li><a class="dropdown-item" href="#">Blue Light Category 2</a></li>
-                                <li><a class="dropdown-item" href="#">Blue Light Category 3</a></li>
-                                <li><a class="dropdown-item" href="#">Blue Light Category 4</a></li>
-                                <li><a class="dropdown-item" href="#">Blue Light Category 5</a></li>
+                                <li><a class="dropdown-item" href="#">Blue Light Black Sunglasses</a></li>
+                                <li><a class="dropdown-item" href="#">Blue Light White Sunglasses</a></li>
+                                <li><a class="dropdown-item" href="#">Blue Light Yellow Sunglasses</a></li>
+                                <li><a class="dropdown-item" href="#">Blue Light Brown Sunglasses</a></li>
+                                <li><a class="dropdown-item" href="#">Blue Light Green Sunglasses</a></li>
                             </ul>
                         </li>
                         <li class="nav-item">
