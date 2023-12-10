b01e714343595fd253994b65e00fded2b5d8194f
MZaman8 <148326306+MZaman8@users.noreply.github.com>
2023-12-10 23:03:15 +0000

    Add files via upload
    navbar with core functionanility for the mvp without term 2 wip content (all commented rather than deleted)

---
+++ mvp-navbar.php
@@ -0,0 +1,158 @@
+<header>
+        <!-- added bootstrap navbar utility classes -->
+        <nav class="navbar navbar-expand-sm w-100">
+
+            <!-- using container-fluid for responsiveness -->
+            <div class="container-fluid">
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMenuItems" aria-controls="navbarMenuItems" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <a href="homepage.php" class="navbar-brand logo">
+                    <img src="shaded logo.png" alt="Shaded Logo">
+                </a>
+                <div class="collapse navbar-collapse" id="navbarMenuItems">
+
+                    <!-- navbar to the left of the search box -->
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                     <!--   <li class="nav-item dropdown"> 
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Men
+                            </a>-->
+            					 <li class="nav-item">
+                            <a class="nav-link" href="shopping.php">Products</a>
+                        		</li>
+                        <!--    <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Men's Black Sunglasses</a></li>
+                                <li><a class="dropdown-item" href="#">Men's White Sunglasses</a></li>
+                                <li><a class="dropdown-item" href="#">Men's Yellow Sunglasses</a></li>
+                                <li><a class="dropdown-item" href="#">Men's Brown Sunglasses</a></li>
+                                <li><a class="dropdown-item" href="#">Men's Green Sunglasses</a></li>
+                            </ul> -->
+                        <!--</li>-->
+                        <li class="nav-item dropdown">
+                           <!-- <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Women
+                            </a>
+                            <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Women's Black Sunglasses</a></li>
+                                <li><a class="dropdown-item" href="#">Women's White Sunglasses</a></li>
+                                <li><a class="dropdown-item" href="#">Women's Yellow Sunglasses</a></li>
+                                <li><a class="dropdown-item" href="#">Women's Brown Sunglasses</a></li>
+                                <li><a class="dropdown-item" href="#">Women's Green Sunglasses</a></li>
+                            </ul>
+                        </li>-->
+            					<li class="nav-item">
+                            <a class="nav-link" href="Contactus.php">Contact Us</a>
+                        		</li>
+                        <li class="nav-item dropdown">
+                         <!--   <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Unisex
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Unisex Black Sunglasses</a></li>
+                                <li><a class="dropdown-item" href="#">Unisex White Sunglasses</a></li>
+                                <li><a class="dropdown-item" href="#">Unisex Yellow Sunglasses</a></li>
+                                <li><a class="dropdown-item" href="#">Unisex Brown Sunglasses</a></li>
+                                <li><a class="dropdown-item" href="#">Unisex Green Sunglasses</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Prescription
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Prescription Black Sunglasses</a></li>
+                                <li><a class="dropdown-item" href="#">Prescription White Sunglasses</a></li>
+                                <li><a class="dropdown-item" href="#">Prescription Yellow Sunglasses</a></li>
+                                <li><a class="dropdown-item" href="#">Prescription Brown Sunglasses</a></li>
+                                <li><a class="dropdown-item" href="#">Prescription Green Sunglasses</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Blue Light
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Blue Light Black Sunglasses</a></li>
+                                <li><a class="dropdown-item" href="#">Blue Light White Sunglasses</a></li>
+                                <li><a class="dropdown-item" href="#">Blue Light Yellow Sunglasses</a></li>
+                                <li><a class="dropdown-item" href="#">Blue Light Brown Sunglasses</a></li>
+                                <li><a class="dropdown-item" href="#">Blue Light Green Sunglasses</a></li>
+                            </ul>
+                        </li> -->
+                        <li class="nav-item">
+                            <a class="nav-link" href="aboutUs.php">About Us</a>
+                        </li>
+                    </ul>
+
+                    <!-- search box -->
+<!--                    <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" id="mySearchInput">
+                        <button class="btn btn-outline-bg" type="submit">
+                            <a href="#">
+                                <i class="fas fa-search"></i>
+                            </a>
+                        </button>
+                    </form>
+-->
+                    <!-- navbar to the right of the search box -->
+                    <ul class="navbar-nav mw-auto mb-2 mb-lg-0">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <i class="fas fa-user"></i> <!-- Assuming a user icon for admin/user -->
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-end">
+                                <li><a class="dropdown-item" href="accountPage.php">My Profile</a></li>
+                                <li><a class="dropdown-item" href="order-history.php">My Orders</a></li>
+                                <li><a class="dropdown-item"  name="nav-logout-button" >Logout</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <i class="fas fa-lock"></i> <!-- Assuming a lock icon for log in/sign up -->
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-end">
+                 <!--               <li><a class="dropdown-item" href="login.php">Log In</a></li>
+                                <li><a class="dropdown-item" href="signup.php">Sign Up</a></li> --> <!-- for later implementation -->
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <i class="fas fa-heart"></i>
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-end">
+                            <li><a class="dropdown-item" >View Wishlist</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <i class="fas fa-shopping-cart"></i>
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-end">
+                                <li><a class="dropdown-item" href="basket.php">View Shopping Cart</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+ <?php
+if(isset($_POST['nav-logout-button'])){
+	session_start();
+
+	session_unset();
+	
+	session_destroy();
+	
+	header("Location:login.php");
+	exit();
+                                
+                                
+                                }
+                                
+                                
+                                ?>
+
+    </header>
