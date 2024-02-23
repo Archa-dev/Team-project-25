e155e3f86b6d6ca50f6947e8e43400c3fc31cf6f
noorulain9 <148326760+noorulain9@users.noreply.github.com>
2024-02-23 15:01:42 +0000

    Update login.php
    I styled the login page and tested it , working alright

--- login.php
+++ login.php
@@ -1,111 +1,193 @@
+<?php
+require_once('connectdb.php');
+//session_start(); 
+if(isset($_POST['logsub'])){
+  $user=$_POST['username'];
+  $pass=$_POST['password'];
+
+
+if($user==null || $pass==null){
+  echo("enter username and password");
+}
+else{
+  $login=$db->prepare('SELECT password FROM logindetails WHERE username=?');
+  $login->bindParam(1,$user);
+
+  $login->execute();
+  $Apass=$login->fetch();
+
+  if($login->rowCount()>0){
+    if(password_verify($pass,$Apass['password'])){
+      //session_start();          
+      $sessionid=$db->prepare('SELECT * FROM logindetails WHERE username=?');
+      $sessionid->bindParam(1,$user);
+      
+      $sessionid->execute();
+      $Asessionid=$sessionid->fetch();
+
+      $_SESSION["username"]=$user;
+      $_SESSION["user_id"]=$Asessionid['user_id'];
+      $_SESSION["authorization_level"]=$Asessionid['authorization_level'];
+
+      if($_SESSION["authorization_level"]=="admin"){
+        header("Location:admin-homepage.php");
+        exit(); 
+      }
+      else if($_SESSION["authorization_level"]=="customer"){
+        $Csessionid=$db->prepare('SELECT customer_id FROM customerdetails WHERE user_id=?');
+        $Csessionid->bindParam(1,$_SESSION['user_id']);
+        $Csessionid->execute();
+        $cid=$Csessionid->fetchColumn();
+        
+        $_SESSION['customer_id']=$cid;
+        header("Location:homepage.php");
+        exit();
+      }
+    }else{
+      echo('incorrect password');
+    }
+  }else{
+    echo('incorrect user');
+  }
+}
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8" />
   <title>Login</title>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+
+  <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
       integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
       crossorigin="anonymous">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"> -->
+
+  <script src="https://kit.fontawesome.com/58e0ebdcbf.js" crossorigin="anonymous"></script>
+  
  <style>
-html {
-  font-size: 80%;
-  scroll-behavior: smooth;
-}
-
-
-
-
-body {
-  font-family: "Century Gothic", sans-serif;
-  background-color: #ffffff;
+html, body{
   margin: 0;
   padding: 0;
+  font-family: "Century Gothic", sans-serif;
   box-sizing: border-box;
-  outline: none;
-  border: none;
-  text-decoration: none;
-  text-transform: capitalize;
-  transition: .2s linear;
-  height: 100vh;
+  height: 100%;
 }
+body{
+            margin: 0;
+            padding: 0;
+            position: relative;
+        }
 
-.login-form {
-  width: 100%;
-  max-width: 600px;
-  padding: 20px;
-  margin: auto;
-  border: 1px solid #ccc;
-  border-radius: 8px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.4);
-  text-align: center;
-  margin-top: 12.5%;
-}
-
-
-.login-form h1 {
-  font-size: 210%; 
-  margin-bottom: 20px;
-}
-
-.login-form h2 {
-  font-size: 125%; 
-  text-align: left; 
-}
-
-.login-form label {
+.container{
+  width : 100%;
+  height : 100vh;
+  background-image:  url(images/8.jpeg);
+  /* linear-gradient(rgba(0, 59, 70, 0.8), rgba(0, 59, 70, 0.8)) */ /* If want to color the background image Green */
+  background-position: centre;
+  background-size: cover;
+  position: relative;
+  z-index: 0;
   
-  margin-top: 10px;
-  text-align: left; 
 }
-
-.login-form input[type="text"],
-.login-form input[type="password"] {
+.form-box{
+  width: 80%;
+  max-width: 400px;
+  position:absolute;
+  top: 50%;
+  left: 0;
+  transform: translate(20%,-50%);
+  /* background: #fff; */
+  /*to change the color of login box make */
+  padding: 4px 2px 6px;
+  text-align: center;
+  border-radius: 10px;
+  box-shadow: 0px 0px 30px 5px ; 
+  
+}
+.form-box h1{
+  font-size: 30px;
+  margin-bottom: 60px;
+  color: #003B46;
+  position: relative;
+}
+.form-box h1::after{
+  content: '';
+  width: 30px;
+  height: 4px;
+  border-radius: 3px;
+  background: #003B46;
+  position: absolute;
+  bottom: -12px;
+  left: 50%;
+  transform: translateX(-50%);
+}
+.form-box a{
+  color: #003B46;
+}
+.form-box a:hover{
+  color: #07575B;
+}
+.input-group{
+  height : 150px;
+}
+.input-field{
+  background: #eaeaea;
+  margin: 15px 0;
+  border-radius: 3px;
+  display: flex;
+  align-items: center;
+}
+input{
   width: 100%;
-  padding: 10px;
-  margin-bottom: 10px;
-  border: 1px solid grey;
-  border-radius: 8px;
-  font-size: 100%;
-  opacity: 0.7;
+  background: transparent;
+  border: 0;
+  outline: 0;
+  padding: 18px 15px ;
 }
-
-.login-form input[type="submit"] {
-  background-color: darkgrey;
+.input-field i{
+  margin-left: 15px;
+  color: #999;
+}
+.btn-field {
+  width : 100%;
+  display: flex;
+  justify-content:center;
+}
+.btn-field button{
+  flex-basis: 48%;
+  background: #003B46;
   color: #fff;
-  border: none;
-  border-radius: 8px;
+  height: 40px;
+  border-radius: 20px;
+  border : 0 ;
+  outline: 0;
   cursor: pointer;
-  padding: 10px 20px;
-  margin-top: 10px;
+  transition: backgroud 1s;
+  margin-bottom: 50px;
 }
-
-.login-form input[type="submit"]:hover {
-  background-color: grey;
-}
-
-
-.login-form input[type="submit"]:hover {
-  background-color: grey;
+.btn-field button:hover{
+  background-color: #07575B;
 }
 
 .footer {
-            background-color: #fff;
+  background: rgba(255, 255, 255, 0.6);
             color: grey;
-            padding: 10px;
+            padding: 0px;
+            height: 1cm;
             text-align: center;
             position: fixed;
             bottom: 0;
             left: 0;
             width: 100%;
-            font-size: 14px;
+            font-size: 12px;
             box-shadow: 0 -5px 10px rgba(0, 0, 0, 0.1);
         }
 
 .social-icons a {
             margin: 0 20px;
             color: grey;
-            font-size: 14px;
+            font-size: 12px;
         }
 
 .terms-links a {
@@ -114,26 +196,32 @@ body {
     text-decoration: none;
 }
 
-.terms-links a:hover {
-    text-decoration: underline; /* underlining on hover  */
-    color: #000; /*hover color  */
-}
  </style>
 </head>
 <body>
-  <form method="post" action="login.php" class="login-form">
-    <h1>Login</h1>
-    <h2>Username:</h2>
-    <input type="text" name="username">
-    <h2>Password:</h2>
-    <input type="password" name="password"><br>
-    <input type="submit" name="submit">
-    <input type="hidden" name="logsub" value="TRUE">
-    <br><br>
-    <a href="signup.php">Don't have an account?</a>
-  </form>
-  
-  <div class="container-fluid">
+  <div class = "container">
+    <div class = "form-box">
+      <h1>Login</h1>
+      <form method="post" action="login.php" class="login-form">
+        <div class = "input-group">
+          <div class = "input-field">
+            <i class="fa-solid fa-user"></i>
+            <input type="text" name="username" placeholder="Username">
+          </div>
+
+          <div class = "input-field">
+            <i class="fa-solid fa-lock"></i>
+            <input type="password" name="password" placeholder="Password">
+          </div>
+        </div>
+        <div class="btn-field">
+          <button type = "submit"  >Log In</button>
+          <input type="hidden" name="logsub" value="TRUE">
+        </div>
+        <a href="signup.php">Don't have an account?</a>
+       </form>
+    </div>
+    <div class="container-fluid">
         <footer class="footer">
             <div class="row">
                 <div class="col-md-4">
@@ -158,74 +246,14 @@ body {
                 </div>
             </div>
         </footer>
-    </div>
-    <?php
-    require_once('connectdb.php');
-    session_start(); 
-    if(isset($_POST['logsub'])){
-      $user=$_POST['username'];
-      $pass=$_POST['password'];
+    </div> 
+</form>
+
     
-    
-    if($user==null || $pass==null){
-      echo("enter username and password");
-    }
-    else{
-      $login=$db->prepare('SELECT password FROM logindetails WHERE username=?');
-      $login->bindParam(1,$user);
+  </div>
+ 
+ 
 
-      $login->execute();
-      $Apass=$login->fetch();
-
-      if($login->rowCount()>0){
-        if(password_verify($pass,$Apass['password'])){
-          echo('success');
-          //session_start();          
-          $sessionid=$db->prepare('SELECT * FROM logindetails WHERE username=?');
-          $sessionid->bindParam(1,$user);
-          
-          $sessionid->execute();
-          $Asessionid=$sessionid->fetch();
-        
-
-          $_SESSION["username"]=$user;
-          $_SESSION["user_id"]=$Asessionid['user_id'];
-          $_SESSION["authorization_level"]=$Asessionid['authorization_level'];
-        
-          echo(var_dump($_SESSION));
-          
-
-          if(  $_SESSION["authorization_level"]=="admin"){
-            header("Location:admin-homepage.php");
-            exit(); 
-          }
-          else if(  $_SESSION["authorization_level"]=="customer"){
-          $Csessionid=$db->prepare('SELECT customer_id FROM customerdetails WHERE user_id=?');
-          $Csessionid->bindParam(1,$_SESSION['user_id']);
-          $Csessionid->execute();
-          $cid=$Csessionid->fetchColumn();
-          
-          $_SESSION['customer_id']=$cid;
-          header("Location:homepage.php");
-          exit();
-        
-        }
-        else{
-          echo('incorrect password');
-        }
-
-
-      }else{
-        echo('incorrect user');
-      }
-
-    }
-  }
-
-
-
-  }
-    ?>
 
 </body>
 </html>
