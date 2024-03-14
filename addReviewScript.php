8d5da09a3b8e5dfef64c98cf88462abd52bd726d
EthanDockerty <220155830@aston.ac.uk>
2024-03-14 22:35:50 +0000

    adding a review now deletes any others from the same account (prevents multiple reviews on same item)

--- addReviewScript.php
+++ addReviewScript.php
@@ -1,11 +1,14 @@
 <?php
     session_start();
     require_once('connectdb.php');
-    // $customerid = $_SESSION['customer_id'];
-    $customerid = 1;
+    $customerid = $_SESSION['customer_id'];
     $productSelected = $_POST['productSelected'];
     $rating = $_POST['rating'];
     $review = $_POST['review'];
+    $removeDupe = $db->prepare('DELETE FROM productreviews WHERE product_id = ? AND user_id = ?');
+    $removeDupe->bindParam(1, $productSelected);
+    $removeDupe->bindParam(2, $customerid);
+    $removeDupe->execute();
     $addReview = $db->prepare('INSERT INTO productreviews VALUES(?,?,?,?)');
     $addReview->bindParam(1, $productSelected);
     $addReview->bindParam(2, $customerid);
