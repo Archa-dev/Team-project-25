2d304b22540c1574437c6693270a46795ac4c1bc
maryam1452 <148329301+maryam1452@users.noreply.github.com>
2024-03-18 21:35:34 +0000

    Update FAQs.php
    Added Favicon Code - Maryam

--- FAQs.php
+++ FAQs.php
@@ -40,7 +40,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['logout'])) {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    
+    <link rel="shortcut icon" href="images/Updatedfavicon.png" type="image/png">
     <title>FAQs - SHADED</title>
     <style>
 
