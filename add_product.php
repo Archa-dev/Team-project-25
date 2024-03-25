893501eaf83b1866daa8c9d7ac08caa13d876698
EthanDockerty <220155830@aston.ac.uk>
2024-03-25 12:50:32 +0000

    add product

--- add_product.php
+++ add_product.php
@@ -8,8 +8,8 @@ $productColor = $_POST['productColor'];
 var_dump($productName, $productPrice, $productCategory, $productColor);
 
 $stmt = $db->prepare("INSERT INTO productdetails (product_name, price, category, colour, stock) VALUES (?, ?, ?, ?, 20)");
-$stmt->bindParam(1, $productName, PDO::PARAM_STR);
-$stmt->bindParam(2, $productPrice, PDO::PARAM_INT);
-$stmt->bindParam(3, $productCategory, PDO::PARAM_STR);
-$stmt->bindParam(4, $productColor, PDO::PARAM_STR);
+$stmt->bindParam(1, $productName);
+$stmt->bindParam(2, $productPrice);
+$stmt->bindParam(3, $productCategory);
+$stmt->bindParam(4, $productColor);
 $stmt->execute();
