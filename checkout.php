3879b3f2f59f340748b02c58d29c37bafb4854f2
maryam1452 <148329301+maryam1452@users.noreply.github.com>
2024-03-28 07:52:35 +0000

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -46,7 +46,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['logout'])) {
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         
-        <link rel="shortcut icon" href="images/Updatedfavicon.png" type="image/png">
+         <link rel="shortcut icon" href="images/updatedFavicon.png" type="image/png">
 
 <style>
 
